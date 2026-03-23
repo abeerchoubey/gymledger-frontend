a7766f00ceed3ca66d2e77fad94cbd192b7eec6e
Abeer Choubey <sentinel.loqs@proton.me>
2026-03-23 18:14:57 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https:://api.gymledger.in.net/api';
+const API_BASE_URL = 'https://api.gymledger.in.net/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
